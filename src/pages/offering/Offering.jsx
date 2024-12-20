4e1122615723283f1e5edbc7b2f82df2c088ebf2
ddgrana2 <dgr2010@gmail.com>
2024-12-20 17:03:09 -0500

    Update page title text from Insights to Our Offering

--- src/pages/offering/Offering.jsx
+++ src/pages/offering/Offering.jsx
@@ -7,36 +7,8 @@ function Offering() {
     <PageLayout
       title={
         <Box sx={{ textAlign: 'center' }}>
-          <Typography 
-            component="span"
-            sx={{ 
-              fontSize: { 
-                xs: 'calc(2rem * 1.2)',
-                sm: 'calc(2.46rem * 1.2)',
-                md: 'calc(3.45rem * 1.2)'
-              },
-              fontWeight: 700,
-              color: '#1618FF',
-              display: 'inline'
-            }}
-          >
-            Our{' '}
-          </Typography>
-          <Typography 
-            component="span"
-            sx={{ 
-              fontSize: { 
-                xs: 'calc(2rem * 1.2)',
-                sm: 'calc(2.46rem * 1.2)',
-                md: 'calc(3.45rem * 1.2)'
-              },
-              fontWeight: 700,
-              color: '#FFFFFF',
-              display: 'inline'
-            }}
-          >
-            Offering
-          </Typography>
+          <Typography>Our{' '}</Typography>
+          <Typography>Offering</Typography>
         </Box>
       }
       subtitle="Discover valuable resources and insights about working in Europe."

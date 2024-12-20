ca33c7ba72d53f490762bb86347c88f24e890e70
ddgrana2 <dgr2010@gmail.com>
2024-12-20 12:33:59 -0500

    Add gradient section below navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -20,90 +20,90 @@ const NavButton = styled(Button)(({ theme }) => ({
 
 function Navbar() {
   return (
-    <AppBar position="static" sx={{ 
-      bgcolor: 'transparent', 
-      boxShadow: 0,
-      marginBottom: '0'
-    }}>
-      <Box 
-        sx={{ 
-          width: '100%', 
-          height: '100px',
-          position: 'relative',
-          overflow: 'hidden'
-        }}
-      >
-        {/* Background Image */}
-        <img 
-          src="./images/header.png"
-          alt="Header"
-          style={{ 
-            width: '100%',
-            height: '100%',
-            objectFit: 'cover',
-            position: 'absolute',
-            zIndex: 1
+    <>
+      <AppBar position="static" sx={{ 
+        bgcolor: '#1621F2',
+        boxShadow: 0,
+        marginBottom: 0
+      }}>
+        <Box 
+          sx={{ 
+            width: '100%', 
+            height: '50px',
+            position: 'relative',
+            overflow: 'hidden'
           }}
-        />
-
-        {/* Logo superpuesto */}
-        <Box sx={{ 
-          position: 'absolute',
-          top: '50%',
-          transform: 'translateY(-50%)',
-          left: '20px',
-          height: '160px',
-          zIndex: 2,
-          display: 'flex',
-          alignItems: 'center'
-        }}>
-          <Link to="/">
-            <img 
-              src="images/logo.png"
-              alt="BlueBridge Logo"
-              style={{ 
-                height: '160px',
-                width: 'auto',
-                objectFit: 'contain',
-                background: 'transparent'
-              }}
-            />
-          </Link>
-        </Box>
-
-        {/* Botones superpuestos */}
-        <Container maxWidth="md" sx={{ 
-          position: 'absolute', 
-          top: '50%',
-          transform: 'translateY(-50%)',
-          left: 0, 
-          right: 0,
-          zIndex: 2,
-          bgcolor: 'transparent'
-        }}>
+        >
+          {/* Logo superpuesto */}
           <Box sx={{ 
-            display: 'flex', 
-            justifyContent: 'center',
-            alignItems: 'center',
-            gap: 2,
+            position: 'absolute',
+            top: '50%',
+            transform: 'translateY(-50%)',
+            left: '20px',
+            height: '80px',
+            zIndex: 2,
+            display: 'flex',
+            alignItems: 'center'
+          }}>
+            <Link to="/">
+              <img 
+                src="images/logo.png"
+                alt="BlueBridge Logo"
+                style={{ 
+                  height: '45px',
+                  width: 'auto',
+                  objectFit: 'contain',
+                  background: 'transparent'
+                }}
+              />
+            </Link>
+          </Box>
+
+          {/* Botones superpuestos */}
+          <Container maxWidth="md" sx={{ 
+            position: 'absolute', 
+            top: '50%',
+            transform: 'translateY(-50%)',
+            left: 0, 
+            right: 0,
+            zIndex: 2,
             bgcolor: 'transparent'
           }}>
-            <NavButton component={Link} to="/">
-              Home
-            </NavButton>
-            <NavButton component={Link} to="/about">
-              About Us
-            </NavButton>
-            <NavButton component={Link} to="/insights">
-              Insights
-            </NavButton>
-            <NavButton component={Link} to="/contact">
-              Contact
-            </NavButton>
-          </Box>
-        </Container>
-      </Box>
-    </AppBar>
+            <Box sx={{ 
+              display: 'flex', 
+              justifyContent: 'center',
+              alignItems: 'center',
+              gap: 2,
+              bgcolor: 'transparent'
+            }}>
+              <NavButton component={Link} to="/">
+                Home
+              </NavButton>
+              <NavButton component={Link} to="/about">
+                About Us
+              </NavButton>
+              <NavButton component={Link} to="/insights">
+                Insights
+              </NavButton>
+              <NavButton component={Link} to="/contact">
+                Contact
+              </NavButton>
+            </Box>
+          </Container>
+        </Box>
+      </AppBar>
+
+      {/* Sección de degradado */}
+      <Box 
+        sx={{ 
+          height: '150px', // 3 veces la altura del toolbar (50px * 3)
+          width: '100%',
+          background: 'linear-gradient(180deg, #414AF2 0%, #FFFFFF 100%)',
+          position: 'relative',
+          zIndex: 1
+        }}
+      />
+    </>
   );
 }
 
